a81aa2627cefe5330b00abad6967e121d54b8260
swelf <golishkin@gmail.com>
2022-08-03 13:20:14 +0300

    added "neutron" feature flag requirement

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -1,2 +1,7 @@
 pub mod query;
 pub mod types;
+
+// This is a signal, such that any contract that imports these helpers will only run on the
+// neutron blockchain
+#[no_mangle]
+extern "C" fn requires_neutron() {}
