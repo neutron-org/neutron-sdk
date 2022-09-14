5dc35c939667b3365ecb679fb22133dace11a43b
Albert Andrejev <albert.andrejev@gmail.com>
2022-09-14 15:30:42 +0300

    Fix test

--- contracts/neutron_interchain_queries/src/testing/tests.rs
+++ contracts/neutron_interchain_queries/src/testing/tests.rs
@@ -61,6 +61,7 @@ fn build_registered_query_response(
             update_period: 0,
             last_submitted_result_local_height,
             last_submitted_result_remote_height: 0,
+            deposit: 100,
         },
     };
     match param {
