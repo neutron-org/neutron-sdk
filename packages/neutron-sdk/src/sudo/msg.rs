69a357e819f38ce04e9a568e1655f812949921e9
swelf <golishkin@gmail.com>
2022-10-14 16:59:51 +0300

    derive

--- packages/neutron-sdk/src/sudo/msg.rs
+++ packages/neutron-sdk/src/sudo/msg.rs
@@ -53,7 +53,7 @@ pub enum SudoMsg {
 /// TransferSudoMsg is a sudo response payload for a native ibc transfer
 /// SudoMsg for ibc transfer has fewer methods then SudoMsg for ica txs
 /// so we describe standalone type to not confuse users with useless variants
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum TransferSudoMsg {
     Response {
