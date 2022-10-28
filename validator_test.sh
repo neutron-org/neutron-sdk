ea8d19504f16abb687ec438ff50fca46d915364a
Sergey Ratiashvili <serg.s.r@gmail.com>
2022-10-28 16:06:01 +0200

    feat: add cosmoshub stuff

--- validator_test.sh
+++ validator_test.sh
@@ -1,5 +1,22 @@
 args=("$@")
 
+echo "What network you going to interact:"
+echo "1. CosmosHub"
+echo "2. Juno"
+
+read -N 1 i
+echo $i
+
+DENOMS=([1]="atom" [2]="junox")
+TARGET_DENOMS=([1]="uatom" [2]="ujunox")
+ICA_LENGTH=([1]="65" [2]="63")
+REMOTE_EXPLORER=([1]="https://explorer.theta-testnet.polypore.xyz" [2]="https://testnet.juno.explorers.guru")
+
+if [[ -z ${DENOMS[$i]} ]]
+then
+    echo "No such network"
+fi
+
 BIN=neutrond
 CONTRACT=${args[0]}
 CONNECTION_ID=${args[1]}
@@ -7,11 +24,11 @@ CONNECTION_ID=${args[1]}
 NEUTRON_KEY_NAME=validator_test
 INTERCHAIN_ACCOUNT_ID=version1
 GAS_PRICES=0.01untrn
-TARGET_VALIDATOR=junovaloper18wgy6hy6yv3fvevl5pyfn7cvzx3t5use2vssnf
-TARGET_DENOM=ujunox
+TARGET_VALIDATORS=([1]="cosmosvaloper1mngvkkhm6g7nqxh4hcv8hjxvgax4m8xujzt964" [2]="junovaloper18wgy6hy6yv3fvevl5pyfn7cvzx3t5use2vssnf")
+TARGET_VALIDATOR=${TARGET_VALIDATORS[$i]}
+TARGET_DENOM=${TARGET_DENOMS[$i]}
 EXPLORER_URL=http://23.109.159.28:3333/
 FAUCET_URL=http://23.109.159.28/
-NEUTRON_NODE_ADDRESS=127.0.0.1
 
 if [ $# != "2" ]
 then
@@ -97,19 +114,28 @@ echo ""
 echo "Register interchain account"
 RES=$(${BIN} tx wasm execute ${CONTRACT_ADDRESS} "{\"register\": {\"connection_id\": \"${CONNECTION_ID}\", \"interchain_account_id\": \"${INTERCHAIN_ACCOUNT_ID}\"}}" --from $NEUTRON_KEY_NAME  -y --chain-id ${NEUTRON_CHAIN_ID} --output json --broadcast-mode=block --gas-prices ${GAS_PRICES} --gas 1000000)
 echo "Waiting for registering account..."
-sleep 60
 
+j=40
+while [[ $j -gt 0 ]]
+do
+    ((j--))
+    RES=$(neutrond query wasm contract-state smart ${CONTRACT_ADDRESS} "{\"interchain_account_address_from_contract\":{\"interchain_account_id\":\"${INTERCHAIN_ACCOUNT_ID}\"}}" --chain-id ${NEUTRON_CHAIN_ID} --output json 2>/dev/null)
+    ICA_ADDRESS=$(echo $RES | jq -r '.data | .[0]')
+    if [ ${#ICA_ADDRESS} = ${ICA_LENGTH[$i]} ]
+    then
+	break
+    fi
+    sleep 5
+done
 
-RES=$(neutrond query wasm contract-state smart ${CONTRACT_ADDRESS} "{\"interchain_account_address_from_contract\":{\"interchain_account_id\":\"${INTERCHAIN_ACCOUNT_ID}\"}}" --chain-id ${NEUTRON_CHAIN_ID} --output json)
-ICA_ADDRESS=$(echo $RES | jq -r '.data | .[0]')
-if [ ${#ICA_ADDRESS} != "63" ]
+if [ ${#ICA_ADDRESS} != ${ICA_LENGTH[$i]} ]
 then
     echo "Can't get ICA address"
     exit
 fi
 echo "ICA address: $ICA_ADDRESS"
 echo ""
-echo "Please send 0.02 junox to $ICA_ADDRESS You can get use faucet from "
+echo "Please send 0.02 ${DENOM[$i]} to $ICA_ADDRESS"
 echo "hit enter when you are ready"
 read
 echo ""
@@ -121,20 +147,22 @@ then
     echo "Delegation failed"
 fi
 echo "Waiting for delegation..."
-sleep 30;
 
-echo ""
-echo "Checking acknowledgement"
-RES=$(${BIN} query wasm contract-state smart ${CONTRACT_ADDRESS} "{\"acknowledgement_result\":{\"interchain_account_id\":\"${INTERCHAIN_ACCOUNT_ID}\", \"sequence_id\": 1}}" --chain-id ${NEUTRON_CHAIN_ID} --output json)
-if [ "$RES" != "{\"data\":{\"success\":[\"/cosmos.staking.v1beta1.MsgDelegate\"]}}" ]
-then
-    echo "Error: Acknowledgement has not been received"
-    exit
-fi
-echo "Acknowledgement has  been received"
-echo ""
-echo "Now you can check your delegation here https://testnet.juno.explorers.guru/account/$ICA_ADDRESS"
-echo "Hit return to exit"
-read
-
+j=40
+while [[ $j -gt 0 ]]
+do
+    ((j--))
+    RES=$(${BIN} query wasm contract-state smart ${CONTRACT_ADDRESS} "{\"acknowledgement_result\":{\"interchain_account_id\":\"${INTERCHAIN_ACCOUNT_ID}\", \"sequence_id\": 1}}" --chain-id ${NEUTRON_CHAIN_ID} --output json 2>/dev/null)
+    if [ "$RES" = "{\"data\":{\"success\":[\"/cosmos.staking.v1beta1.MsgDelegate\"]}}" ]
+    then
+	echo "Acknowledgement has  been received"
+	echo ""
+	echo "Now you can check your delegation here ${REMOTE_EXPLORER[$i]}/account/$ICA_ADDRESS"
+	echo "Hit return to exit"
+	read
+	exit
+    fi
+    sleep 5
+done
+echo "Error: Acknowledgement has not been received"
 
