b3944f856756e24708c9573398da2e6f6476d49a
Sergey Ratiashvili <serg.s.r@gmail.com>
2022-11-08 14:16:51 +0100

    feat: separate scripts

--- validator_test.sh
+++ validator_test.sh
@@ -1,4 +1,22 @@
 args=("$@")
+
+if [ $# != "1" ]
+then
+    echo "Usage: ./validator_test.sh  [connection-id]"
+    exit
+fi
+
+if [[ ! -f /tmp/contract_address.tmp ]]
+then
+    echo "Run ./validator_test_upload_contract.sh first"
+    exit
+fi
+
+
+CONTRACT_ADDRESS=$(cat /tmp/contract_address.tmp)
+echo "Contract address: $CONTRACT_ADDRESS"
+
+
 ## Set up envs
 echo "What network you going to interact:"
 echo "1. CosmosHub"
@@ -17,8 +35,7 @@ then
 fi
 
 BIN=neutrond
-CONTRACT=${args[0]}
-CONNECTION_ID=${args[1]}
+CONNECTION_ID=${args[0]}
 
 NEUTRON_KEY_NAME=validator_test
 INTERCHAIN_ACCOUNT_ID=version1
@@ -28,18 +45,6 @@ TARGET_VALIDATOR=${TARGET_VALIDATORS[$i]}
 TARGET_DENOM=${TARGET_DENOMS[$i]}
 EXPLORER_URL=http://23.109.159.28:3333/
 FAUCET_URL=http://23.109.159.28/
-
-if [ $# != "2" ]
-then
-    echo "Usage: ./validator_test.sh [path_to_wasm_artifact] [connection-id]"
-    exit
-fi
-
-if [[ ! -f $CONTRACT ]]
-then
-    echo "Artifact file doesn't exists"
-    exit
-fi
 
 if ! command -v $BIN &> /dev/null
 then
@@ -87,36 +92,6 @@ echo "Make sure tx is passed by going to $EXPLORER_URL/accounts/$NEUTRON_ADDRESS
 echo "Hit enter when ready"
 read
 
-## Upload contract
-echo "Upload the queries contract"
-RES=$(${BIN} tx wasm store ${CONTRACT} --from ${NEUTRON_KEY_NAME} --gas 50000000 --chain-id ${NEUTRON_CHAIN_ID} --broadcast-mode=block --gas-prices ${GAS_PRICES}  -y --output json)
-CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
-if [ $CONTRACT_CODE_ID = "null" ]
-then
-    echo "Can't get code id"
-    echo "$RES"
-    exit
-fi
-
-echo "Contract code id: $CONTRACT_CODE_ID"
-echo ""
-
-## Instantiate contract
-echo "Instantiate the contract"
-INIT_CONTRACT='{}'
-RES=$(${BIN} tx wasm instantiate $CONTRACT_CODE_ID "$INIT_CONTRACT" --from $NEUTRON_KEY_NAME --admin ${NEUTRON_ADDRESS} -y --chain-id ${NEUTRON_CHAIN_ID} --output json --broadcast-mode=block --label "init"  --gas-prices ${GAS_PRICES} --gas auto --gas-adjustment 1.4)
-CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
-echo "Contract address: $CONTRACT_ADDRESS"
-
-
-if [ $CONTRACT_ADDRESS = "null" ]
-then
-    echo "Can't get contract address"
-    echo "$RES"
-    exit
-fi
-echo ""
-
 ## Set ibc fees
 echo "Set IBC fees"
 RES=$(${BIN} tx wasm execute ${CONTRACT_ADDRESS} "{\"set_fees\": {\"ack_fee\": \"2000\", \"recv_fee\": \"2000\",\"timeout_fee\": \"2000\", \"denom\": \"${INTERCHAIN_ACCOUNT_ID}\"}}" --from $NEUTRON_KEY_NAME  -y --chain-id ${NEUTRON_CHAIN_ID} --output json --broadcast-mode=block --gas-prices ${GAS_PRICES} --gas 1000000)
@@ -131,7 +106,7 @@ fi
 
 ## Fund contract to be able to pay fees
 echo "Fund the contract to pay for IBC fees"
-RES=$(${BIN} tx bank send $NEUTRON_KEY_NAME ${CONTRACT_ADDRESS} 20000untrn --chain-id neutron-devnet-1 --gas-prices ${GAS_PRICES})
+RES=$(${BIN}tx bank send $NEUTRON_KEY_NAME ${CONTRACT_ADDRESS} 20000untrn --chain-id neutron-devnet-1 --gas-prices 0${GAS_PRICES})
 echo ""
 
 ## Register interchain account
