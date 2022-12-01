8553d277a30e92e3b919871df53d9984bbbacae9
Albert Andrejev <albert.andrejev@gmail.com>
2022-12-01 17:02:58 +0200

    Update tests

--- scripts/test_crud.sh
+++ scripts/test_crud.sh
@@ -5,36 +5,49 @@ NEUTRON_DIR=${NEUTRON_DIR:-../neutron}
 HOME=${NEUTRON_DIR}/data/test-1/
 HOME2=${NEUTRON_DIR}/data/test-2/
 KEY=demowallet1
+FAUCET=demowallet3
 ADMIN=neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2
-RLY2=neutron17dtl0mjt3t77kpuhg2edqzjpszulwhgzcdvagh
-BIN=neutrond
+RLY2=cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs
 
-RES=$(${BIN} tx wasm store ${CONTRACT} --from ${KEY} --gas 50000000  --chain-id ${CHAINID} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
+BIN=neutrond
+GAIAD_BIN=gaiad
+
+TEST_WALLET=test_wallet
+
+yes | ${BIN} keys add ${TEST_WALLET} --home ${HOME} --keyring-backend=test
+TEST_ADDR=$(${BIN} keys show ${TEST_WALLET} --keyring-backend test -a --home ${HOME})
+${BIN} tx bank send ${FAUCET} ${TEST_ADDR} 100000000stake --chain-id ${CHAINID} --home ${HOME} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
+
+
+RES=$(${BIN} tx wasm store ${CONTRACT} --from ${TEST_ADDR} --gas 50000000  --chain-id ${CHAINID} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
 ICQ_CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
 echo $RES
 echo $ICQ_CONTRACT_CODE_ID
 
 INIT_ICQ_CONTRACT='{}'
 
-RES=$(${BIN} tx wasm instantiate $ICQ_CONTRACT_CODE_ID "$INIT_ICQ_CONTRACT" --from ${KEY} --admin ${ADMIN} -y --chain-id ${CHAINID} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm instantiate $ICQ_CONTRACT_CODE_ID "$INIT_ICQ_CONTRACT" --from ${TEST_ADDR} --admin ${ADMIN} -y --chain-id ${CHAINID} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME} --node tcp://127.0.0.1:16657)
 echo $RES
 ICQ_CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
 echo $ICQ_CONTRACT_ADDRESS
 
-RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"register_balance_query":{"connection_id":"connection-0","addr":"neutron17dtl0mjt3t77kpuhg2edqzjpszulwhgzcdvagh","denom":"stake","update_period":10}}' --from ${KEY}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
+${BIN} tx bank send ${TEST_WALLET} ${ICQ_CONTRACT_ADDRESS} 20000000stake --chain-id ${CHAINID} --home ${HOME} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
+
+
+RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"register_balance_query":{"connection_id":"connection-0","addr":"cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs","denom":"stake","update_period":10}}' --from ${TEST_ADDR}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
 echo $RES
 
 ${BIN} q interchainqueries registered-queries  --chain-id ${CHAINID}  --home ${HOME} --node tcp://127.0.0.1:16657
 
-RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"update_interchain_query":{"query_id":1,"new_keys":[{"path":"staking","key":"AhTzV/fuS4r96weXQrLQCkGAufddAnN0YWtl"}]}}' --from ${KEY}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"update_interchain_query":{"query_id":1,"new_keys":[{"path":"staking","key":"AhTzV/fuS4r96weXQrLQCkGAufddAnN0YWtl"}]}}' --from ${TEST_ADDR}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
 echo $RES
 
 ${BIN} q interchainqueries registered-queries  --chain-id ${CHAINID}  --home ${HOME} --node tcp://127.0.0.1:16657
 
-RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"remove_interchain_query":{"query_id":1}}' --from ${KEY}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"remove_interchain_query":{"query_id":1}}' --from ${TEST_ADDR}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
 echo $RES
 
-RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"register_balance_query":{"connection_id":"connection-0","addr":"neutron17dtl0mjt3t77kpuhg2edqzjpszulwhgzcdvagh","denom":"stake","update_period":10}}' --from ${KEY}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm execute $ICQ_CONTRACT_ADDRESS '{"register_balance_query":{"connection_id":"connection-0","addr":"cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs","denom":"stake","update_period":10}}' --from ${TEST_ADDR}  -y --chain-id ${CHAINID} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
 echo $RES
 
 ${BIN} q interchainqueries registered-queries  --chain-id ${CHAINID}  --home ${HOME} --node tcp://127.0.0.1:16657
--- scripts/test_ibc_transfer.sh
+++ scripts/test_ibc_transfer.sh
@@ -2,30 +2,39 @@ CONTRACT=./artifacts/ibc_transfer.wasm
 CHAINID=test-1
 NEUTRON_DIR=${NEUTRON_DIR:-../neutron}
 HOME=${NEUTRON_DIR}/data/test-1/
-HOME2=${NEUTRON_DIR}/data/test-2/
 KEY=demowallet1
 ADMIN=neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2
-BIN=neutrond
 
-RES=$(${BIN} tx wasm store ${CONTRACT} --from ${KEY} --gas 50000000  --chain-id ${CHAINID} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
+BIN=neutrond
+GAIAD_BIN=gaiad
+
+FAUCET=demowallet3
+TEST_WALLET=test_wallet
+
+yes | ${BIN} keys add ${TEST_WALLET} --home ${HOME} --keyring-backend=test
+TEST_ADDR=$(${BIN} keys show ${TEST_WALLET} --keyring-backend test -a --home ${HOME})
+${BIN} tx bank send ${FAUCET} ${TEST_ADDR} 100000000stake --chain-id ${CHAINID} --home ${HOME} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
+
+
+RES=$(${BIN} tx wasm store ${CONTRACT} --from ${TEST_ADDR} --gas 50000000  --chain-id ${CHAINID} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME} --node tcp://127.0.0.1:16657)
 TRANSFER_CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
 echo $RES
 echo $TRANSFER_CONTRACT_CODE_ID
 
 INIT_TRANSFER_CONTRACT='{}'
 
-RES=$(${BIN} tx wasm instantiate $TRANSFER_CONTRACT_CODE_ID "$INIT_TRANSFER_CONTRACT" --from ${KEY} --admin ${ADMIN} -y --chain-id ${CHAINID} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm instantiate $TRANSFER_CONTRACT_CODE_ID "$INIT_TRANSFER_CONTRACT" --from ${TEST_ADDR} --admin ${ADMIN} -y --chain-id ${CHAINID} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME} --node tcp://127.0.0.1:16657)
 echo $RES
 TRANSFER_CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
 echo $TRANSFER_CONTRACT_ADDRESS
 
-${BIN} tx bank send demowallet1 ${TRANSFER_CONTRACT_ADDRESS} 10000stake --chain-id ${CHAINID} --home ${HOME} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
+${BIN} tx bank send ${FAUCET} ${TRANSFER_CONTRACT_ADDRESS} 10000stake --chain-id ${CHAINID} --home ${HOME} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
 
 
 echo "Tranfer coins from test-1 to test-2"
 RES=$(${BIN} tx wasm execute $TRANSFER_CONTRACT_ADDRESS \
-    '{"send":{"to":"neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2","amount":"1000", "denom": "stake", "channel": "channel-0"}}' \
-    --from ${KEY}  -y \
+    '{"send":{"to":"cosmos10h9stc5v6ntgeygf5xf945njqq5h32r53uquvw","amount":"1000", "denom": "stake", "channel": "channel-0"}}' \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAINID} \
     --output json \
     --broadcast-mode=block \
@@ -48,7 +57,7 @@ FAILURES_BEFORE_TEST=$(${BIN} q contractmanager failures $TRANSFER_CONTRACT_ADDR
 echo "Turn off sudo handler"
 RES=$(${BIN} tx wasm execute $TRANSFER_CONTRACT_ADDRESS \
     '{"integration_tests_set_sudo_failure_mock":{}}' \
-    --from ${KEY}  -y \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAINID} \
     --output json \
     --broadcast-mode=block \
@@ -61,8 +70,8 @@ echo $RES | jq
 
 echo "Send coins from test-2 to test-1"
 RES=$(${BIN} tx wasm execute $TRANSFER_CONTRACT_ADDRESS \
-    '{"send":{"to":"neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2","amount":"1000", "denom": "stake", "channel": "channel-0"}}' \
-    --from ${KEY}  -y \
+    '{"send":{"to":"cosmos10h9stc5v6ntgeygf5xf945njqq5h32r53uquvw","amount":"1000", "denom": "stake", "channel": "channel-0"}}' \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAINID} \
     --output json \
     --broadcast-mode=block \
@@ -77,7 +86,7 @@ echo "Wait for message to be processed and turn on sudo handler"
 sleep 10
 RES=$(${BIN} tx wasm execute $TRANSFER_CONTRACT_ADDRESS \
     '{"integration_tests_unset_sudo_failure_mock":{}}' \
-    --from ${KEY}  -y \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAINID} \
     --output json \
     --broadcast-mode=block \
