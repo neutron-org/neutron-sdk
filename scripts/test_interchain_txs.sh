6cb3d540316a1d85d6699645cc0b932518fa38bf
Albert Andrejev <albert.andrejev@gmail.com>
2022-12-01 16:25:03 +0200

    fixes

--- scripts/test_interchain_txs.sh
+++ scripts/test_interchain_txs.sh
@@ -1,4 +1,5 @@
 BIN=neutrond
+GAIAD_BIN=gaiad
 
 CONTRACT=./artifacts/neutron_interchain_txs.wasm
 
@@ -9,14 +10,20 @@ NEUTRON_DIR=${NEUTRON_DIR:-../neutron}
 HOME_1=${NEUTRON_DIR}/data/test-1/
 HOME_2=${NEUTRON_DIR}/data/test-2/
 
-ADDRESS_1=neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2
-ADDRESS_2=neutron10h9stc5v6ntgeygf5xf945njqq5h32r54rf7kf
+FAUCET=demowallet3
+ADDRESS_2=cosmos10h9stc5v6ntgeygf5xf945njqq5h32r53uquvw
 ADMIN=neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2
 
-VAL2=neutronvaloper1qnk2n4nlkpw9xfqntladh74w6ujtulwnqshepx
+VAL2=cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9
+TEST_WALLET=test_wallet
+
+yes | ${BIN} keys add ${TEST_WALLET} --home ${HOME_1} --keyring-backend=test
+TEST_ADDR=$(${BIN} keys show ${TEST_WALLET} --keyring-backend test -a --home ${HOME_1})
+${BIN} tx bank send ${FAUCET} ${TEST_ADDR} 100000000stake --chain-id ${CHAIN_ID_1} --home ${HOME_1} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
+
 
 # Upload the queries contract
-RES=$(${BIN} tx wasm store ${CONTRACT} --from ${ADDRESS_1} --gas 50000000  --chain-id ${CHAIN_ID_1} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm store ${CONTRACT} --from ${TEST_ADDR} --gas 50000000  --chain-id ${CHAIN_ID_1} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
 CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
 echo $RES
 echo $CONTRACT_CODE_ID
@@ -24,14 +31,14 @@ echo $CONTRACT_CODE_ID
 # Instantiate the contract
 INIT_CONTRACT='{}'
 echo "Instantiate"
-RES=$(${BIN} tx wasm instantiate $CONTRACT_CODE_ID "$INIT_CONTRACT" --from ${ADDRESS_1} --admin ${ADMIN} -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --gas auto --gas-adjustment 1.4 --home ${HOME_1} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm instantiate $CONTRACT_CODE_ID "$INIT_CONTRACT" --from ${TEST_ADDR} --admin ${ADMIN} -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --gas auto --gas-adjustment 1.4 --home ${HOME_1} --node tcp://127.0.0.1:16657)
 CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
 echo $CONTRACT_ADDRESS
 
 #Register interchain account
-RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS "{\"register\": {\"connection_id\": \"connection-0\", \"interchain_account_id\": \"test\"}}" --from ${ADDRESS_1}  -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS "{\"register\": {\"connection_id\": \"connection-0\", \"interchain_account_id\": \"test\"}}" --from ${TEST_ADDR}  -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
 echo $RES
-sleep 10
+sleep 20
 
 RES=$(curl http://127.0.0.1:1316/wasm/contract/$CONTRACT_ADDRESS/smart/eyJpbnRlcmNoYWluX2FjY291bnRfYWRkcmVzc19mcm9tX2NvbnRyYWN0Ijp7ImludGVyY2hhaW5fYWNjb3VudF9pZCI6InRlc3QifX0\=?encoding\=base64 | jq -r ".result.smart")
 echo $RES
@@ -39,11 +46,11 @@ ICA_ADDRESS=$(echo $RES | base64 --decode | jq -r ".[0]")
 echo $ICA_ADDRESS
 
 #Send some money to ICA
-RES=$(${BIN} tx bank send ${ADDRESS_2} ${ICA_ADDRESS} 10000stake --chain-id ${CHAIN_ID_2}  --broadcast-mode=block --gas-prices 0.0025stake -y --output json --keyring-backend test --home ${HOME_2} --node tcp://127.0.0.1:26657)
+RES=$(${GAIAD_BIN} tx bank send ${ADDRESS_2} ${ICA_ADDRESS} 10000stake --chain-id ${CHAIN_ID_2}  --broadcast-mode=block --gas-prices 0.0025stake -y --output json --keyring-backend test --home ${HOME_2} --node tcp://127.0.0.1:26657)
 echo $RES
 
 #Delegate
-RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS "{\"delegate\": {\"interchain_account_id\": \"test\", \"validator\": \"${VAL2}\", \"amount\":\"5000\",\"denom\":\"stake\"}}" --from ${ADDRESS_1}  -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS "{\"delegate\": {\"interchain_account_id\": \"test\", \"validator\": \"${VAL2}\", \"amount\":\"5000\",\"denom\":\"stake\"}}" --from ${TEST_ADDR}  -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
 echo $RES
 
 sleep 7;
@@ -60,7 +67,7 @@ FAILURES_BEFORE_TEST=$(${BIN} q contractmanager failures $CONTRACT_ADDRESS \
 echo "Turn off sudo handler"
 RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS \
     '{"integration_tests_set_sudo_failure_mock":{}}' \
-    --from ${ADDRESS_1}  -y \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAIN_ID_1} \
     --output json \
     --broadcast-mode=block \
@@ -74,7 +81,7 @@ echo $RES | jq
 echo "Delegate"
 RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS \
     "{\"delegate\": {\"interchain_account_id\": \"test\", \"validator\": \"${VAL2}\", \"amount\":\"5000\",\"denom\":\"stake\"}}" \
-    --from ${ADDRESS_1}  -y \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAIN_ID_1} \
     --output json \
     --broadcast-mode=block \
@@ -89,7 +96,7 @@ echo "Wait for message to be processed and turn on sudo handler"
 sleep 10
 RES=$(${BIN} tx wasm execute $CONTRACT_ADDRESS \
     '{"integration_tests_unset_sudo_failure_mock":{}}' \
-    --from ${ADDRESS_1}  -y \
+    --from ${TEST_ADDR}  -y \
     --chain-id ${CHAIN_ID_1} \
     --output json \
     --broadcast-mode=block \
--- scripts/test_kv_query_result.sh
+++ scripts/test_kv_query_result.sh
@@ -1,4 +1,5 @@
 BIN=neutrond
+GAIAD_BIN=gaiad
 
 CONTRACT=./artifacts/neutron_interchain_queries.wasm
 
@@ -10,23 +11,23 @@ HOME_1=${NEUTRON_DIR}/data/test-1/
 HOME_2=${NEUTRON_DIR}/data/test-2/
 
 USERNAME_1=demowallet1
+FAUCET=demowallet3
 USERNAME_2=demowallet2
-KEY_2=$(neutrond keys show ${USERNAME_2} -a --keyring-backend test --home ${HOME_2})
+KEY_2=$(gaiad keys show ${USERNAME_2} -a --keyring-backend test --home ${HOME_2})
 ADMIN=$(neutrond keys show ${USERNAME_1} -a --keyring-backend test --home ${HOME_1})
 
-TARGET_ADDRESS=neutron1mjk79fjjgpplak5wq838w0yd982gzkyf8fxu8u
-VAL2=neutronvaloper1qnk2n4nlkpw9xfqntladh74w6ujtulwnqshepx
+TARGET_ADDRESS=cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs
+VAL2=cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9
 TEST_WALLET=test_wallet
 
 yes | ${BIN} keys add ${TEST_WALLET} --home ${HOME_1} --keyring-backend=test
 TEST_ADDR=$(${BIN} keys show ${TEST_WALLET} --keyring-backend test -a --home ${HOME_1})
-
-${BIN} tx bank send ${USERNAME_1} ${TEST_ADDR} 100000000stake --chain-id ${CHAIN_ID_1} --home ${HOME_1} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
+${BIN} tx bank send ${FAUCET} ${TEST_ADDR} 100000000stake --chain-id ${CHAIN_ID_1} --home ${HOME_1} --node tcp://localhost:16657 --keyring-backend test -y --gas-prices 0.0025stake --broadcast-mode=block
 
 
 # Upload the queries contract
 echo "Upload the queries contract"
-RES=$(${BIN} tx wasm store ${CONTRACT} --from ${USERNAME_1} --gas 50000000  --chain-id ${CHAIN_ID_1} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm store ${CONTRACT} --from ${TEST_ADDR} --gas 50000000  --chain-id ${CHAIN_ID_1} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
 QUERIES_CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
 echo $RES
 echo $QUERIES_CONTRACT_CODE_ID
@@ -35,14 +36,14 @@ echo $QUERIES_CONTRACT_CODE_ID
 echo "Instantiate the queries contract"
 INIT_QUERIES_CONTRACT='{}'
 
-RES=$(${BIN} tx wasm instantiate $QUERIES_CONTRACT_CODE_ID "$INIT_QUERIES_CONTRACT" --from ${USERNAME_1} --admin ${ADMIN} -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME_1} --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm instantiate $QUERIES_CONTRACT_CODE_ID "$INIT_QUERIES_CONTRACT" --from ${TEST_ADDR} --admin ${ADMIN} -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME_1} --node tcp://127.0.0.1:16657)
 echo $RES
 QUERIES_CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
 echo $QUERIES_CONTRACT_ADDRESS
 
 # Send coins from USERNAME_1 to QUERIES_CONTRACT_ADDRESS to perform register_interchain_query message
-echo "Send coins from ${USERNAME_1} to ${QUERIES_CONTRACT_ADDRESS} to perform register_interchain_query message"
-echo $(${BIN} tx bank send ${USERNAME_1} ${QUERIES_CONTRACT_ADDRESS} 10000000stake -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 300000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
+echo "Send coins from ${TEST_ADDR} to ${QUERIES_CONTRACT_ADDRESS} to perform register_interchain_query message"
+echo $(${BIN} tx bank send ${TEST_ADDR} ${QUERIES_CONTRACT_ADDRESS} 10000000stake -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 300000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
 
 
 # Register a query for KEY_2 balance
