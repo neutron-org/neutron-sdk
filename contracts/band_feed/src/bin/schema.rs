e7d579fbeae2d51cacebb0e7bf6071cf8574ef2f
nhpd <dmitry.klpv@gmail.com>
2022-12-07 22:21:33 +0800

    scaffold band_feed contract

---
+++ contracts/band_feed/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use band_feed::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
