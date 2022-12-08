32a9b66041c5b609f49d1c9fcf0a38711c7b16cd
nhpd <dmitry.klpv@gmail.com>
2022-12-08 19:19:29 +0800

    feat: add more code to ask price

--- contracts/band_feed/src/contract.rs
+++ contracts/band_feed/src/contract.rs
@@ -1,8 +1,9 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{Binary, Coin, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult, Uint64};
 use cw2::set_contract_version;
-// use cw_band::
+use neutron_sdk::bindings::msg::{NeutronMsg, IbcFee};
+use neutron_sdk::bindings::types::ProtobufAny;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
@@ -31,7 +32,7 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
   match msg {
-      ExecuteMsg::AskPrice{} => execute_ask_price(deps, env)
+      ExecuteMsg::AskPrice{} => execute_ask_price(deps, env),
   }
 }
 
@@ -42,32 +43,124 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> NeutronResult<Binary>{
     }
 }
 
-fn execute_ask_price(deps: DepsMut, env: Env) -> NeutronResult<Response<NeutronMsg>> {
+fn execute_ask_price(mut deps: DepsMut, env: Env) -> NeutronResult<Response<NeutronMsg>> {
+    let cosmos_msg = ask_price_msg()?;
+    // We use a submessage here because we need the process message reply to save
+    // the outgoing IBC packet identifier for later.
+    let submsg = msg_with_sudo_callback(
+        deps.branch(),
+        cosmos_msg,
+        SudoPayload {
+            port_id: get_port_id(env.contract.address.as_str(), &interchain_account_id),
+            message: "message".to_string(),
+        },
+    )?;
 
+    // TODO: is this correct? ask_price and create query right away!
+    let update_period = 15;
+    let msg = new_price_request_query_msg(connection_id, request_id, update_period)?;
+
+    Ok(Response::default().add_message(msg).add_submessages(vec![submsg]))
 }
 
 fn query_price(deps: Deps<InterchainQueries>, env: Env) -> NeutronResult<Binary>{
-
+    unimplemented!()
 }
 
-// https://docs.rs/cw-band/0.1.0-alpha.2/cw_band/struct.OracleRequestPacketData.html
-// pub struct OracleRequestPacketData {
-//     pub client_id: String,
-//     pub oracle_script_id: Uint64,
-//     pub calldata: Vec<u8>,
-//     pub ask_count: Uint64,
-//     pub min_count: Uint64,
-//     pub fee_limit: Vec<Coin>,
-//     pub prepare_gas: Uint64,
-//     pub execute_gas: Uint64,
-// }
-// fn ask_price_msg() -> NeutronMsg {
-    // TODO
-    // cw_band::OracleRequestPacketData{
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct OracleRequestPacketData {
+    #[prost(string, tag = "1")]
+    pub client_id: ::prost::alloc::string::String,
+    #[prost(uint64, tag = "2")]
+    pub oracle_script_id: u64,
+    #[prost(bytes = "vec", tag = "3")]
+    pub calldata: ::prost::alloc::vec::Vec<u8>,
+    #[prost(uint64, tag = "4")]
+    pub ask_count: u64,
+    #[prost(uint64, tag = "5")]
+    pub min_count: u64,
+    #[prost(message, repeated, tag = "6")]
+    pub fee_limit: ::prost::alloc::vec::Vec<::prost::v1beta1::Coin>,
+    #[prost(uint64, tag = "7")]
+    pub prepare_gas: u64,
+    #[prost(uint64, tag = "8")]
+    pub execute_gas: u64,
+}
 
-    // }
-    // return NeutronMsg
-// }
+fn ask_price_msg() -> StdResult<NeutronMsg> {
+    let price_msg = OracleRequestPacketData{
+        client_id: "TODO".to_string(),
+        oracle_script_id: 0,
+        calldata: vec![1,2,3],
+        ask_count: 1,
+        min_count: 1,
+        fee_limit: vec![Coin::new(1, "uatom")],
+        prepare_gas: 10000,
+        execute_gas: 1000,
+    };
+    let mut buf = Vec::new();
+    buf.reserve(price_msg.encoded_len());
+    if let Err(e) = price_msg.encode(&mut buf) {
+        return Err(StdError::generic_err(format!("Encode error: {}", e)));
+    }
+
+    let any_msg = ProtobufAny {
+        // FIXME
+        type_url: "/oracle.types.Request".to_string(),
+        value: Binary::from(buf),
+    };
+
+    // FIXME
+    let fee = IbcFee{
+        recv_fee: vec![Coin::new(100, "uatom")],
+        ack_fee: vec![Coin::new(100, "uatom")],
+        timeout_fee: vec![Coin::new(100, "uatom")],
+    };
+
+    Ok(NeutronMsg::submit_tx(
+        connection_id,
+        interchain_account_id.clone(),
+        vec![any_msg],
+        "".to_string(),
+        timeout.unwrap_or(DEFAULT_TIMEOUT_SECONDS),
+        fee,
+    ))
+}
+
+pub fn new_price_request_query_msg(
+    connection_id: String,
+    request_id: Uint64,
+    update_period: u64,
+) -> NeutronResult<NeutronMsg> {
+    let mut kv_keys: Vec<KVKey> = Vec::with_capacity(1);
+
+    let supply_key = create_request_key(request_id)?;
+
+    let kv_key = KVKey {
+        path: BAND_STORE_KEY.to_string(),
+        key: Binary(supply_key),
+    };
+
+    kv_keys.push(kv_key);
+
+    NeutronMsg::register_interchain_query(QueryPayload::KV(kv_keys), connection_id, update_period)
+}
+
+const BAND_STORE_KEY: &str = "oracle";
+// Keys for stored bandchain requests
+// https://github.com/bandprotocol/chain/blob/v2.4.1/x/oracle/types/keys.go#L46
+const REQUEST_KEY: u8 = 0x01;
+
+// Creates Cosmos-sdk storage key for request with given request_id in bandchain
+// https://github.com/bandprotocol/chain/blob/v2.4.1/x/oracle/types/keys.go#L63
+fn create_request_key<AddrBytes: AsRef<[u8]>>(
+    request_id: Uint64,
+) -> NeutronResult<Vec<u8>> {
+    let mut key: Vec<u8> = vec![REQUEST_KEY];
+    key.extend_from_slice(request_id.to_be_bytes.as_slice());
+
+    Ok(key)
+}
 
 #[cfg(test)]
 mod tests {}
