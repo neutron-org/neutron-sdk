a1563b8b2420f942254617ef887ac4294d787985
Murad Karammaev <muradkarammaev@gmail.com>
2023-01-11 17:46:09 +0200

    TotalBurnedNeutronsAmount: Coins → Coin

--- packages/neutron-sdk/src/query/total_burned_neutrons.rs
+++ packages/neutron-sdk/src/query/total_burned_neutrons.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct TotalBurnedNeutronsAmountResponse {
-    pub coins: Vec<Coin>,
+    pub coin: Coin,
 }
 
 /// Returns total amount of burned neutron fees
