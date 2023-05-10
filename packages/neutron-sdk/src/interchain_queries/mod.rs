01d42f88c250f0bd5110335987a63fbfe1f62dee
sotnikov-s <ssotnikov93@gmail.com>
2023-05-10 16:38:14 +0300

    remove new_register_interchain_query_msg

--- packages/neutron-sdk/src/interchain_queries/mod.rs
+++ packages/neutron-sdk/src/interchain_queries/mod.rs
@@ -3,6 +3,4 @@ pub mod queries;
 pub mod types;
 pub mod v045;
 
-pub use queries::{
-    check_query_type, get_registered_query, new_register_interchain_query_msg, query_kv_result,
-};
+pub use queries::{check_query_type, get_registered_query, query_kv_result};
--- packages/neutron-sdk/src/interchain_queries/queries.rs
+++ packages/neutron-sdk/src/interchain_queries/queries.rs
@@ -1,15 +1,10 @@
 use crate::bindings::query::{
     NeutronQuery, QueryRegisteredQueryResponse, QueryRegisteredQueryResultResponse,
 };
-use crate::interchain_queries::types::{
-    KVReconstruct, QueryPayload, QueryType, TransactionFilterItem,
-};
+use crate::errors::error::NeutronResult;
+use crate::interchain_queries::types::{KVReconstruct, QueryType};
 use crate::NeutronError;
-use crate::{
-    bindings::{msg::NeutronMsg, types::KVKey},
-    errors::error::NeutronResult,
-};
-use cosmwasm_std::{Deps, DepsMut, Env};
+use cosmwasm_std::Deps;
 
 /// Checks **actual** query type is **expected** query type
 pub fn check_query_type(actual: QueryType, expected: QueryType) -> NeutronResult<()> {
@@ -19,31 +14,6 @@ pub fn check_query_type(actual: QueryType, expected: QueryType) -> NeutronResult
         });
     }
     Ok(())
-}
-
-#[allow(clippy::too_many_arguments)]
-/// Creates a message to register an Interchain Query with provided params
-pub fn new_register_interchain_query_msg(
-    _deps: DepsMut<NeutronQuery>,
-    _env: Env,
-    connection_id: String,
-    query_type: QueryType,
-    kv_keys: Vec<KVKey>,
-    transactions_filter: Vec<TransactionFilterItem>,
-    update_period: u64,
-) -> NeutronResult<NeutronMsg> {
-    match query_type {
-        QueryType::KV => NeutronMsg::register_interchain_query(
-            QueryPayload::KV(kv_keys),
-            connection_id,
-            update_period,
-        ),
-        QueryType::TX => NeutronMsg::register_interchain_query(
-            QueryPayload::TX(transactions_filter),
-            connection_id,
-            update_period,
-        ),
-    }
 }
 
 /// Queries registered query info
