1d9ace9f601acfdb51073e8f720e5958768501bd
Maurits Bos <mb.brains@gmail.com>
2023-07-14 20:14:40 +0200

    more bad queries

--- contracts/neutron_interchain_queries/src/contract.rs
+++ contracts/neutron_interchain_queries/src/contract.rs
@@ -19,8 +19,10 @@ use crate::state::Config;
 use crate::state::CONFIG;
 
 use cosmos_sdk_proto::cosmos::tx::v1beta1::{TxBody, TxRaw};
+use cosmos_sdk_proto::traits::MessageExt;
 use cosmwasm_std::{
-    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult, WasmMsg, from_binary,
+    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult,
+    Uint128, WasmMsg, from_binary, Empty,
 };
 use cw2::set_contract_version;
 use prost::Message as ProstMessage;
@@ -28,8 +30,8 @@ use prost::Message as ProstMessage;
 use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg, TransferNftResponse,
 };
 use crate::query_helpers::{new_register_transfer_nft_query_msg, WASM_EXECUTE_MSG_TYPE};
-use crate::state::{NftTransfer};
-use neutron_sdk::bindings::msg::{NeutronMsg};
+use crate::state::{NftTransfer, RECIPIENT_TXS, TRANSFERS};
+use neutron_sdk::bindings::msg::{NeutronMsg, MsgExecuteContractResponse};
 use neutron_sdk::bindings::query::{NeutronQuery, QueryRegisteredQueryResponse};
 use neutron_sdk::bindings::types::{Height};
 use neutron_sdk::interchain_queries::{
@@ -266,7 +268,7 @@ pub fn sudo(deps: DepsMut<NeutronQuery>, env: Env, msg: SudoMsg) -> NeutronResul
         } => sudo_tx_query_result(deps, env, query_id, height, data),
 
         // For handling kv query result
-        SudoMsg::KVQueryResult { query_id } => sudo_kv_query_result(deps, env, query_id),
+        SudoMsg::KVQueryResult { query_id } => panic!(),
         _ => Ok(Response::default()),
     }
 }
@@ -297,14 +299,23 @@ pub fn sudo_tx_query_result(
     // TODO: come up with solution to determine transactions filter type
     match registered_query.registered_query.query_type {
         _ => {
-            // TODO: implements
-            let msg: WasmMsg = body.messages.get(0).unwrap();
-            if let WasmMsg::Execute { contract_addr, msg, funds } = msg {
-                if let cw721_base::ExecuteMsg::TransferNft { recipient, token_id } = from_binary(msg)? 
-                {
-                    
+            let msg = body.messages.get(0).unwrap();
+
+            if let WasmMsg::Execute { contract_addr, msg, funds } = from_binary(&msg.to_bytes().into())? {
+                if let cw721_base::ExecuteMsg::TransferNft { recipient, token_id } = from_binary(&msg)? 
+                { 
+                    let sender = deps.api.addr_validate(recipient.as_str())?;
+                    let contract_addr = deps.api.addr_validate(contract_addr.as_str())?;
+                    let transfer_nft = NftTransfer {
+                        sender: todo!(),
+                        contract_address: contract_addr.to_string(),
+                        token_id,
+                    };
+
+                    return Ok(Response::new().add_attribute("transfer_nft", transfer_nft.into()));
                 }
 
+                
             }
 
             
@@ -356,6 +367,7 @@ fn nft_transfers_from_tx_body(
     // will be ignored to prevent 'out of gas' conditions.
     // Note: in real contracts you will have to somehow save ignored
     // data in order to handle it later.
+    
 
     for msg in tx_body.messages.iter().take(MAX_ALLOWED_MESSAGES) {
         // Skip all messages in this transaction that are not Send messages.
@@ -364,61 +376,16 @@ fn nft_transfers_from_tx_body(
         }
 
         // Parse a Send message and check that it has the required recipient.
-        let transfer_msg: MsgExecuteContract = MsgExecuteContract::(msg.value.as_slice())?;
-        if transfer_msg.to_address == recipient {
-            for coin in transfer_msg.amount {
-                transfers.push(NftTransfer {
-                    sender: transfer_msg.from_address.clone(),
-                    amount: coin.amount.clone(),
-                    denom: coin.denom,
-                    recipient: recipient.to_string(),
-                });
-            }
-        }
+        let transfer_msg: cw721_base::ExecuteMsg<Empty, Empty> = from_binary(msg.value.as_slice());
+        // TODO HOW can i parse this message data into the right interface?
+
+
+        transfers.push(NftTransfer {
+            sender: transfer_msg,
+            contract_address: todo!(),
+            token_id: todo!(),
+        })
     }
     Ok(transfers)
-}
 
-// checks whether there are deposits that are greater then MAX_ALLOWED_TRANSFER.
-fn check_deposits_size(deposits: &Vec<Transfer>) -> StdResult<()> {
-    for deposit in deposits {
-        match deposit.amount.parse::<u64>() {
-            Ok(amount) => {
-                if amount > MAX_ALLOWED_TRANSFER {
-                    return Err(StdError::generic_err(format!(
-                        "maximum allowed transfer is {}",
-                        MAX_ALLOWED_TRANSFER
-                    )));
-                };
-            }
-            Err(error) => {
-                return Err(StdError::generic_err(format!(
-                    "failed to cast transfer amount to u64: {}",
-                    error
-                )));
-            }
-        };
-    }
-    Ok(())
-}
-
-/// sudo_kv_query_result is the contract's callback for KV query results. Note that only the query
-/// id is provided, so you need to read the query result from the state.
-pub fn sudo_kv_query_result(
-    deps: DepsMut<NeutronQuery>,
-    _env: Env,
-    query_id: u64,
-) -> NeutronResult<Response> {
-    deps.api.debug(
-        format!(
-            "WASMDEBUG: sudo_kv_query_result received; query_id: {:?}",
-            query_id,
-        )
-        .as_str(),
-    );
-
-    // TODO: provide an actual example. Currently to many things are going to change
-    // after @pro0n00gler's PRs to implement this.
-
-    Ok(Response::default())
 }
