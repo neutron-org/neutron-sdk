f8a4721ac2788aa9fb4686f3529adddba9a1b4b9
sotnikov-s <ssotnikov93@gmail.com>
2023-12-11 12:32:11 +0300

    add comments for make_stargate_query and create_stargate_msg

--- packages/neutron-sdk/src/stargate/aux.rs
+++ packages/neutron-sdk/src/stargate/aux.rs
@@ -2,6 +2,15 @@ use cosmwasm_std::{Binary, CosmosMsg, Deps, QueryRequest, StdResult};
 use prost_types::Timestamp as TimestampGen;
 use serde::de::DeserializeOwned;
 
+/// makes a stargate query by a given path and req and returns a response deserialised into a
+/// given response model.
+///
+/// * **req** is a proto request model. Most likely it's a result of proto code generation;
+/// * **path** is an RPC request path. Should be one of allowlisted stargate query paths;
+///
+/// Since stargate query results are JSON-encoded instead of protobuf-encoded, the Res is
+/// expected to have a serde::de::DeserializeOwned trait. Why JSON, not proto? See the link:
+/// https://github.com/CosmWasm/wasmd/blob/6f6be7880f1caa666b86aaafea625208d70675dc/x/wasm/keeper/query_plugins.go#L360
 pub(crate) fn make_stargate_query<Req, Res>(deps: Deps, req: Req, path: &str) -> StdResult<Res>
 where
     Req: prost::Message,
@@ -13,6 +22,11 @@ where
     })
 }
 
+/// creates a CosmosMsg::Stargate with given request payload and path.
+///
+/// * **req** is a proto request model. Most likely it's a result of proto code generation;
+/// * **path** is an RPC request path. See Msg service definitions in neutron modules' proto files
+/// for additional info.
 pub(crate) fn create_stargate_msg<Req: prost::Message>(req: Req, path: &str) -> CosmosMsg {
     CosmosMsg::Stargate {
         type_url: path.to_string(),
