19502db977fd82f2dc7a5e81c22f670316db8429
Albert Andrejev <albert.andrejev@gmail.com>
2024-02-12 16:53:44 +0200

    add update gov proposal and update proposal votes functions

--- packages/neutron-sdk/src/interchain_queries/v045/helpers.rs
+++ packages/neutron-sdk/src/interchain_queries/v045/helpers.rs
@@ -165,6 +165,22 @@ pub fn create_gov_proposal_key(proposal_id: u64) -> NeutronResult<Vec<u8>> {
     Ok(key)
 }
 
+/// Creates Cosmos-SDK storage keys for list of proposals
+pub fn create_gov_proposal_keys(proposals_ids: Vec<u64>) -> NeutronResult<Vec<KVKey>> {
+    let mut kv_keys: Vec<KVKey> = Vec::with_capacity(proposals_ids.len());
+
+    for id in proposals_ids {
+        let kv_key = KVKey {
+            path: GOV_STORE_KEY.to_string(),
+            key: Binary(create_gov_proposal_key(id)?),
+        };
+
+        kv_keys.push(kv_key)
+    }
+
+    Ok(kv_keys)
+}
+
 /// Creates Cosmos-SDK governance key for votes for proposal with specific id
 /// <https://github.com/cosmos/cosmos-sdk/blob/35ae2c4c72d4aeb33447d5a7af23ca47f786606e/x/gov/types/keys.go#L48>
 pub fn create_gov_proposal_votes_key(proposal_id: u64) -> NeutronResult<Vec<u8>> {
