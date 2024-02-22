1cbef1ae0dde2a4a96ad01c94b9bc878d94976f3
Albert Andrejev <albert.andrejev@gmail.com>
2024-02-22 13:44:08 +0200

    remove debug output

--- packages/neutron-sdk/src/interchain_queries/queries.rs
+++ packages/neutron-sdk/src/interchain_queries/queries.rs
@@ -36,11 +36,6 @@ pub fn query_kv_result<T: KVReconstruct>(
 ) -> NeutronResult<T> {
     let registered_query_result = get_raw_interchain_query_result(deps, query_id)?;
 
-    deps.api.debug(&format!(
-        "WASMDEBUG: query_kv_result registered_query_result: {:?}",
-        registered_query_result
-    ));
-
     KVReconstruct::reconstruct(&registered_query_result.result.kv_results)
 }
 
