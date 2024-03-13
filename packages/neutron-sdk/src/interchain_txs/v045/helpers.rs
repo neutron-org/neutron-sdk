93ab7c6541329bd6c4e9fec98bd9353615191ec3
quasisamurai <quazisamurai@gmail.com>
2024-03-13 09:46:38 -0300

    rm comment & branch

--- packages/neutron-sdk/src/interchain_txs/v045/helpers.rs
+++ packages/neutron-sdk/src/interchain_txs/v045/helpers.rs
@@ -5,32 +5,14 @@ use cosmos_sdk_proto::{
 use cosmwasm_std::{Binary, StdError, StdResult};
 
 /// Decodes acknowledgement into `Vec<MsgData>` structure
-/// We consider this method as deprecated. Use v047 instead.
 pub fn decode_acknowledgement_response(data: Binary) -> StdResult<Vec<MsgData>> {
-    let tx_msg_data: Result<TxMsgData, _> = TxMsgData::decode(data.as_slice());
-
-    match tx_msg_data {
+    let msg_data: Result<TxMsgData, _> = TxMsgData::decode(data.as_slice());
+    match msg_data {
         Err(e) => Err(StdError::generic_err(format!(
             "Can't decode response: {}",
             e
         ))),
-        Ok(msg) => {
-            if !msg.msg_responses.is_empty() {
-                msg.msg_responses
-                    .into_iter()
-                    .map(|any_msg| {
-                        Ok(MsgData {
-                            msg_type: any_msg.type_url,
-                            data: any_msg.value,
-                        })
-                    })
-                    .collect::<StdResult<Vec<MsgData>>>()
-            } else {
-                // Field `.data` is deprecated since cosmos-sdk v047.
-                // But for backwards compatibility we still allow that. Given function can be used w both v045 & v047
-                #[allow(deprecated)]
-                Ok(msg.data)
-            }
-        }
+        #[allow(deprecated)]
+        Ok(msg) => Ok(msg.data),
     }
 }
