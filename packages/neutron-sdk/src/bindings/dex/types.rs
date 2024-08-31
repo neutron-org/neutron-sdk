f17d7f802538daa392f8a0fa87fc82e68edead05
Julian Compagni Portis <jcompagni@gmail.com>
2024-08-30 20:14:14 -0400

    make some fields optional
    -DepositOptions
    -FailedDeposit

--- packages/neutron-sdk/src/bindings/dex/types.rs
+++ packages/neutron-sdk/src/bindings/dex/types.rs
@@ -81,8 +81,8 @@ pub struct TradePairID {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct FailedDeposit {
-    pub deposit_idx: u64,
-    pub error: String,
+    pub deposit_idx: Option<u64>,
+    pub error: Option<String>,
 }
 
 // TODO implement math for PrecDec
@@ -110,8 +110,8 @@ impl From<String> for PrecDec {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct DepositOptions {
-    pub disable_autoswap: bool,
-    pub fail_tx_on_bel: bool,
+    pub disable_autoswap: Option<bool>,
+    pub fail_tx_on_bel: Option<bool>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
